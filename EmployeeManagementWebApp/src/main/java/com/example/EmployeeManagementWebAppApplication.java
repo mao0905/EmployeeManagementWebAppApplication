dceb4f22eb648be5c51412511158b7ad5806c753
*PvAl4OnO22 <ozawa.mao@ring-tech.jp>
2024-01-17 14:32:06 +0900

    不要な@RestControllerを削除

--- EmployeeManagementWebApp/src/main/java/com/example/EmployeeManagementWebAppApplication.java
+++ EmployeeManagementWebApp/src/main/java/com/example/EmployeeManagementWebAppApplication.java
@@ -2,10 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EmployeeManagementWebAppApplication {
 
 	public static void main(String[] args) {
