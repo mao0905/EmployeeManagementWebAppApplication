9a0957a1fd48904e64e812d703017846caa37916
*PvAl4OnO22 <ozawa.mao@ring-tech.jp>
2024-01-22 12:36:22 +0900

    休憩開始、終了ボタン追加、差分計算で勤務時間、休憩時間を割り出し、勤怠ページに表示

---
+++ EmployeeManagementWebApp/src/main/java/com/example/controller/CalendarController.java
@@ -0,0 +1,16 @@
+package com.example.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalendarController {
+
+    @GetMapping("/hello")
+    public String showHello(Principal principal) {
+        return "/hello";
+    }
+
+}
